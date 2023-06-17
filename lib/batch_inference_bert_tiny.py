69a530910742c8f9a199358a74564886bd90a1e1
anuragdutt <anurag2709@gmail.com>
2023-06-16 22:19:52 -0400

    Adding same sentence const for inference

--- lib/batch_inference_bert_tiny.py
+++ lib/batch_inference_bert_tiny.py
@@ -18,30 +18,29 @@ def generate_random_sentence(size_kb):
 
 
 if __name__ == "__main__":
-    # batch_size = int(sys.argv[1])
-    # print("TimePreModelLoading --", time.time())
-    # model_name = "prajjwal1/bert-tiny"
-    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # tokenizer = BertTokenizer.from_pretrained(model_name)
-    # model = BertForSequenceClassification.from_pretrained(model_name)
-    # model.to(device)
-    # model.eval()
-    # print("TimePostModelLoading --", time.time())
+    batch_size = int(sys.argv[1])
+    print("TimePreModelLoading --", time.time())
+    model_name = "prajjwal1/bert-tiny"
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    tokenizer = BertTokenizer.from_pretrained(model_name)
+    model = BertForSequenceClassification.from_pretrained(model_name)
+    model.to(device)
+    model.eval()
+    print("TimePostModelLoading --", time.time())
 
     sentences = []
     np.random.seed(123456789)
     sentence_const = generate_random_sentence(size_kb=1.1)
-    print(sentence_const)
     count = 64
-    # print("Generated Sentences")
+    print("Generated Sentences")
 
-    # inputs = []
-    # for i in range(0, int(64 / batch_size)):
-    #     input = [sentence_const] * batch_size
-    #     inputs.append(tokenizer(input, padding=True, truncation=True, return_tensors="pt").to(device))
+    inputs = []
+    for i in range(0, int(64 / batch_size)):
+        input = [sentence_const] * batch_size
+        inputs.append(tokenizer(input, padding=True, truncation=True, return_tensors="pt").to(device))
 
-    # print("TimePreModel --", time.time())
-    # for i in range(0, 50):
-    #     for input in inputs:
-    #         outputs = model(**input)
-    # print("TimePostModel --", time.time())
+    print("TimePreModel --", time.time())
+    for i in range(0, 50):
+        for input in inputs:
+            outputs = model(**input)
+    print("TimePostModel --", time.time())
