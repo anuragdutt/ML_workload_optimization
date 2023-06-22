17a2798b25a5722e7168e281f428b64b9f7e711c
anuragdutt <anurag2709@gmail.com>
2023-06-22 19:04:01 -0400

    Making the single color plots

--- stats/3d plot.py
+++ stats/3d plot.py
@@ -2,10 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import pandas as pd
+import sys
 
-dat = pd.read_csv("../archive/merged_csv/resnet_base_dle3_2023_06_16.csv")
+fn = sys.argv[1]
+dat = pd.read_csv("../archive/merged_csv/" + fn)
 dat["Total_Power"] = dat["stats_Total_Avg_Power"]
-dat_d = pd.read_csv("../archive/merged_csv/resnet_dvfs_dle2_2023_06_16.csv")
+# dat_d = pd.read_csv("../archive/merged_csv/resnet_dvfs_dle2_2023_06_16.csv")
 
 b = 16
 tmp = dat[dat.Batch_Size == b]
@@ -13,6 +15,10 @@ cpuf = dat.Cpu_Freq.unique()
 cpuf = cpuf[~np.isnan(cpuf)]
 gpuf = dat.Gpu_Freq.unique()
 gpuf = gpuf[~np.isnan(gpuf)]
+
+# cmap = 'Reds'
+cmap = plt.cm.get_cmap('Reds')
+cmap = cmap.reversed()
 
 tmppl = []
 for c in cpuf:
@@ -26,12 +32,20 @@ tmpp = pd.DataFrame(tmppl, columns = ["Cpu_Freq", "Gpu_Freq", "stats_Energy"])
 X = tmpp.Gpu_Freq
 Y = tmpp.Cpu_Freq
 Z = tmpp.stats_Energy
+normalization_factor = 0.7  # Adjust this value to control the rate of normalization
+Z_min = Z.min()
+Z_max = Z.max()
+Z_range = Z_max - Z_min
+
+norm = plt.Normalize(Z_min, Z_min + Z_range * normalization_factor)
+
+
 # Create a 3D figure and axis
 fig = plt.figure(figsize=(10, 8))
 ax = fig.add_subplot(111, projection='3d')
 
 # Create the surface plot
-surf = ax.plot_trisurf(X, Y, Z, cmap='viridis')
+surf = ax.plot_trisurf(X, Y, Z, cmap = cmap, norm = norm)
 
 # Add color bar
 fig.colorbar(surf)
